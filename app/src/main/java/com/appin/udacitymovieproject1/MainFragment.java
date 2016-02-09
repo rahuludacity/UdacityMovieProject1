4b8e838f69880c23b1e2c1d75c770d94d15951e5
Rahul <dandamudi.rahul@yahoo.com>
2016-02-08 20:08:03 -0500

    Intial Changes

--- app/src/main/java/com/appin/udacitymovieproject1/MainFragment.java
+++ app/src/main/java/com/appin/udacitymovieproject1/MainFragment.java
@@ -2,9 +2,11 @@ package com.appin.udacitymovieproject1;
 
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -20,18 +22,17 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
+import com.squareup.otto.Subscribe;
 
 import java.util.List;
 
-import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
-import info.movito.themoviedbapi.model.core.MovieResultsPage;
 
 public class MainFragment extends Fragment implements Constants {
 
     private RecyclerView recyclerView;
     private ProgressDialog pd;
+
 
     @Nullable
     @Override
@@ -41,21 +42,65 @@ public class MainFragment extends Fragment implements Constants {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        MovieEventBus.getInstance().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MovieEventBus.getInstance().unregister(this);
+    }
+
+
+    @Subscribe
+    public void onAsyncTaskResult(MovieAsyncTaskResults event) {
+        if (pd.isShowing()) {
+            pd.dismiss();
+        }
+        if (event.getMoviesList() != null && !event.getMoviesList().isEmpty()) {
+            recyclerView.setAdapter(new MoviesGridAdater(event.getMoviesList()));
+        } else {
+            Toast.makeText(getActivity(), "We are unable to get the information Currently", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setRetainInstance(true);
         pd = new ProgressDialog(getActivity());
         pd.setMessage("Downloading please wait...");
         pd.setCanceledOnTouchOutside(false);
-        new MoviesAsyncTask().execute(MOST_POPULAR);
+        getDetails(MOST_POPULAR);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         recyclerView.setHasFixedSize(true);
+    }
 
+    private void getDetails(String value) {
+        if (isOnline(getActivity())) {
+            new MovieAsyncTask(pd, getResources()).execute(value);
+
+        } else {
+            Toast.makeText(getActivity(), "Please check your network conenction", Toast.LENGTH_SHORT).show();
+        }
     }
 
     protected void sortList(String value) {
-        new MoviesAsyncTask().execute(value);
+        getDetails(value);
+    }
+
+    public boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
     }
 
     public static class PosterHolder extends RecyclerView.ViewHolder {
@@ -64,40 +109,6 @@ public class MainFragment extends Fragment implements Constants {
         public PosterHolder(View itemView) {
             super(itemView);
             imageView = (NetworkImageView) itemView.findViewById(R.id.movieImage);
-        }
-    }
-
-    protected class MoviesAsyncTask extends AsyncTask<String, Void, List<MovieDb>> {
-
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pd.show();
-        }
-
-        @Override
-        protected List<MovieDb> doInBackground(String... params) {
-            TmdbMovies movies = new TmdbApi(getString(R.string.api_key)).getMovies();
-            if (MOST_RATED.equals(params[0])) {
-                MovieResultsPage movie = movies.getTopRatedMovies("en", 10);
-                return movie.getResults();
-            }
-            MovieResultsPage movie = movies.getPopularMovieList("en", 10);
-            return movie.getResults();
-        }
-
-        @Override
-        protected void onPostExecute(List<MovieDb> moviesList) {
-            super.onPostExecute(moviesList);
-            if (pd.isShowing()) {
-                pd.dismiss();
-            }
-            if (moviesList != null && !moviesList.isEmpty()) {
-                recyclerView.setAdapter(new MoviesGridAdater(moviesList));
-            } else {
-                Toast.makeText(getActivity(), "We are unable to get the information Currently", Toast.LENGTH_LONG).show();
-            }
         }
     }
 
@@ -131,6 +142,8 @@ public class MainFragment extends Fragment implements Constants {
 
         @Override
         public void onBindViewHolder(PosterHolder holder, int position) {
+            holder.imageView.setErrorImageResId(R.drawable.error_drawable);
+            holder.imageView.setDefaultImageResId(R.drawable.default_image);
             holder.imageView.setImageUrl("http://image.tmdb.org/t/p/w500" + moviesList.get(position).getPosterPath(), mImageLoader);
             holder.imageView.setTag(moviesList.get(position));
             holder.imageView.setOnClickListener(new View.OnClickListener() {
@@ -141,7 +154,10 @@ public class MainFragment extends Fragment implements Constants {
                     startActivity(i);
                 }
             });
+
+
         }
+
 
         @Override
         public int getItemCount() {
